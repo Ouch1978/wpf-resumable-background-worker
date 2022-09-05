7c2a5c617d2f4b5be5d896bcd2ee07c729e6b1cb
Ouch1978 <ouch1978@hotmail.com>
2022-09-05 23:22:04 +0800

    1. First commit.

---
+++ ResumableBackgroundWorker/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+
+namespace ResumableBackgroundWorker
+{
+    public partial class MainWindow : Window
+    {
+        private BackgroundWorker _myWorker = new BackgroundWorker
+        {
+            WorkerReportsProgress = true,
+            WorkerSupportsCancellation = true
+        };
+
+        private ManualResetEvent _manualReset = new ManualResetEvent(true);
+
+        private int _totalCount = 9999;
+
+        public MainWindow()
+        {
+            this.SourceInitialized += new System.EventHandler(MainWindow_SourceInitialized);
+
+            _myWorker.DoWork += _myWorker_DoWork;
+            _myWorker.ProgressChanged += _myWorker_ProgressChanged;
+            _myWorker.RunWorkerCompleted += _myWorker_RunWorkerCompleted;
+        }
+
+        private void _myWorker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < _totalCount; i++)
+            {
+                if (_myWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                _manualReset.WaitOne();
+
+                Thread.Sleep(20);
+
+                _myWorker.ReportProgress(i++);
+
+            }
+        }
+
+        private void _myWorker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            txtMessage.Text = $"{e.ProgressPercentage.ToString().PadLeft(_totalCount.ToString().Length)}/{_totalCount}";
+        }
+
+        private void _myWorker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            btnStop.IsEnabled = false;
+        }
+
+        private void tglPauseResume_Checked(object sender, RoutedEventArgs e)
+        {
+            if (_myWorker.IsBusy == false)
+            {
+                _myWorker.RunWorkerAsync();
+            }
+            else
+            {
+                _manualReset.Set();
+            }
+        }
+
+        private void tglPauseResume_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _manualReset.Reset();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            _myWorker.CancelAsync();
+        }
+    }
+}
